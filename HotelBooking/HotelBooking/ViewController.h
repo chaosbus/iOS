3c849ecbb1343ee9f4a093bba52b4158c92d61ba
chaosbus <chaosbus@gmail.com>
2015-01-14 17:12:58 +0800

    base version commit

---
+++ HotelBooking/HotelBooking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HotelBooking
+//
+//  Created by Joe on 15/1/14.
+//  Copyright (c) 2015年 Joe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
